f3f17f26068ebaa85444fee09c2c5d723685d82a
Bohdan Turkynewych <tb0hdan@gmail.com>
2017-06-21 21:07:18 +0300

    giphy plugin

---
+++ torpedo_giphy_plugin/giphy.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/tb0hdan/torpedo_registry"
+	common "github.com/tb0hdan/torpedo_common"
+	"github.com/tb0hdan/torpedo_plugins/torpedo_giphy_plugin/giphy"
+	"torpedobot/multibot"
+)
+
+func GiphyProcessMessage(api *torpedo_registry.BotAPI, channel interface{}, incoming_message string) {
+	var message string
+	var richmsg torpedo_registry.RichMessage
+	cu := &common.Utils{}
+	logger := cu.NewLog("giphy-process-message")
+
+	client := giphy.NewClient()
+
+	_, command, message := common.GetRequestedFeature(incoming_message)
+	if command != "" {
+		logger.Printf("Got command %s\n", command)
+		giphyResponse := client.GiphySearch(command)
+		if giphyResponse.Meta.Status == 200 {
+			richmsg = multibot.RichMessage{BarColor: "#36a64f",
+				Text:      command,
+				TitleLink: giphyResponse.Data[0].URL,
+				ImageURL:  giphyResponse.Data[0].Images.OriginalImage.URL}
+			message = ""
+		} else {
+			message = "Your request to Giphy could not be processed"
+		}
+	}
+	api.Bot.PostMessage(channel, message, api, richmsg)
+}
+
+func init() {
+	torpedo_registry.RegisterHandler("giphy", GiphyProcessMessage)
+	torpedo_registry.RegisterHelp("giphy", "Get Giphy.com image")
+}
+
