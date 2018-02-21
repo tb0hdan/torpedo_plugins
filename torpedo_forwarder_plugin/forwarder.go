154fc5c2dd3ffead4bd00b93c8eda88fb79f348b
Bohdan Turkynewych <tb0hdan@gmail.com>
2018-02-22 01:16:52 +0200

    #41: Sample Slack forwarder (to be properly extended for other protocols

---
+++ torpedo_forwarder_plugin/forwarder.go
@@ -0,0 +1,28 @@
+package torpedo_forwarder_plugin
+
+import (
+	"fmt"
+
+	"github.com/nlopes/slack"
+	"github.com/tb0hdan/torpedo_registry"
+)
+
+func ForwarderProcessTextMessage(api *torpedo_registry.BotAPI, channel interface{}, incoming_message string) {
+	apiKey := "xoxb-"
+	destination := "D-"
+	s_channel := fmt.Sprintf("%s", channel)
+	if destination == s_channel {
+		return
+	}
+	account := torpedo_registry.Accounts.GetAccountByAPIKey(apiKey)
+	switch capi := account.API.(type) {
+	case *slack.Client:
+		params := slack.PostMessageParameters{}
+		capi.PostMessage(destination, fmt.Sprintf("%s", channel)+" "+api.UserProfile.Nick+" "+incoming_message, params)
+	}
+	return
+}
+
+func init() {
+	torpedo_registry.Config.RegisterTextMessageHandler("forwarder", ForwarderProcessTextMessage)
+}
